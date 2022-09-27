700d0ff6e1459a7c6b5f5f3c455917f4b385c59b
rruiz88 <roman.ruiz816@gmail.com>
2022-09-27 14:58:38 -0500

    completed

--- script.js
+++ script.js
@@ -1,41 +1,101 @@
-let arr = ["Rock", "Paper", "Scissors"];
-let choice;
+
+let computerSelection;
+let playerSelection;
+let computerScore = 0;
+let playerScore = 0;
+let round = 0;
+
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const outerDiv = document.querySelector('.outer');
+const buttons = document.querySelector('.buttons');
+
+rock.addEventListener("click", () => {
+    playerSelection = "rock";
+    game();
+    console.log("rock clicked");
+});
+
+
+paper.addEventListener("click", () =>{
+    playerSelection = "paper";
+    game();
+    console.log("paper clicked");
+});
+
+scissors.addEventListener("click", () => {
+    playerSelection = "scissors";
+    game();
+    console.log("scissors clicked");
+});
+
+const results = document.querySelector('.results');
+
+
+const btnRestart = document.querySelector('#btnRestart');
+btnRestart.onclick = () => history.go(0);
 
 function getComputerChoice() {
-    let option = Math.floor(Math.random() * arr.length);
-    choice = arr[option];
-    return choice;
+    let pick = Math.floor(Math.random() * 3);
+
+            if (pick === 0) {
+                return "rock";
+            } else if (pick === 1) {
+                return "paper";
+            } else if (pick === 2) {
+                return "scissors";
+            }
 }
-console.log(getComputerChoice());
+
+
 
 function playRound(playerSelection, computerSelection) {
 
-computerSelection.toLowerCase;    
-
-if(playerSelection === computerSelection) {
-    return "Tie!";
-} else if (playerSelection === "rock") {
-    if(computerSelection === "paper") {
-        return "You Lose! Paper covers Rock."
-    } else {
-        return "You Win! Rock crushes Scissors."
-    }
-} else if (playerSelection === "scissors") {
-    if(computerSelection === "rock") {
-        return "You Lose! Rock crushes Scissors."
-    } else {
-        return "You Win! Scissors cuts Paper."
-    }
-} else if (playerSelection === "paper") {
-    if(computerSelection === "scissors") {
-        return "You Lose! Scissors cuts Paper."
-    } else {
-        return "You Win! Paper covers Rock."
-    }
-}
+    if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") ||  (playerSelection === "scissors" && computerSelection === "paper")) {
+        return "You Win!";
+        } else if (playerSelection === computerSelection) {
+        return "It's a tie!";
+        } else {
+        return "You Lose!";
+        }
 
 }
 
-const playerSelection = "paper";
-const computerSelection = getComputerChoice().toLowerCase;
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+    round++;
+    computerSelection = getComputerChoice();
+    if (playRound(playerSelection, computerSelection) === "You Win!") {
+        playerScore++;
+    } else if (playRound(playerSelection, computerSelection) === "You Lose!") {
+        computerScore++;
+    }
+
+    const throws = document.querySelector(".throws")
+            throws.textContent = `Round ${round}! Player throws ${playerSelection}!
+            Computer throws ${computerSelection}!`
+
+            const whoWins = document.querySelector(".whoWins");
+            whoWins.textContent = `${playRound(playerSelection, computerSelection)}`;
+
+            const score = document.querySelector(".score");
+            score.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+
+            const gameOver = document.querySelector(".gameOver");
+            gameOver.textContent = `${winner(playerScore, computerScore)}`;
+}
+
+
+function winner(playerScore, computerScore) { 
+    if (playerScore === 5) {
+        outerDiv.removeChild(buttons);
+        return "Player wins!"
+    } else if (computerScore === 5) {
+        outerDiv.removeChild(buttons);
+        return "Computer wins!"
+    } else {
+        return "Next round!"
+    }
+
+}
+
